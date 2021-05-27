2b1959d4d0dd84de8ded3ac8d633b8028474bd09
Adison25 <emerickadison@gmail.com>
2021-05-27 12:37:42 -0700

    test for subtraction

--- test.py
+++ test.py
@@ -6,6 +6,10 @@ class TestCase(unittest.TestCase):
 
     def test_add_1(self):
         self.assertEqual(example.add(1, 2), 3)
+    
+    
+    def test_add_1(self):
+        self.assertEqual(example.sub(3, 2), 1)
 
 
 if __name__ == '__main__':
